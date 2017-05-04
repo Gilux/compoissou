e711ea306744a75cbaafc01230f28e28529ce642
Gilux <gilux762@gmail.com>
2017-05-04 11:45:49 +0200

    Changement du .gitignore => Dézipper vendor.zip et c/c à la racine de l'application.
    + Création bundle Blog.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new ProjetblogBundle\ProjetblogBundle(),
+            new BlogBundle\BlogBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

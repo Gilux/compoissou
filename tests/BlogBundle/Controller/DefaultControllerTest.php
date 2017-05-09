818fe8a014e8b4cf47fa728f3ece623b1809c0ac
Gilux <gilux762@gmail.com>
2017-05-09 17:19:29 +0200

    Test travis avec dépendances - 9

--- tests/BlogBundle/Controller/DefaultControllerTest.php
+++ tests/BlogBundle/Controller/DefaultControllerTest.php
@@ -6,5 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    public function testIndex()
+    {
+        $client = static::createClient();
 
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('blog', $client->getResponse()->getContent());
+    }
 }
